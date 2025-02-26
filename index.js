dbdc72ef1b4ae8d5d123443d2f2a6f78c262bfe4
mahadi24t <mahadi24t@outlook.com>
2025-02-26 15:22:50 +0600

    nike shoe purchase

---
+++ index.js
@@ -0,0 +1,37 @@
+// let firsname = "Mahadi";
+// let lastName = "Hasan";
+
+// let fullName = firsname + " " + lastName;
+
+// console.log(fullName);
+
+// let name = "Linda"
+// let greeting = "Hi there!";
+
+// let greetLinda = () => {
+//     console.log(greeting + " " + name + '!');
+// };
+// greetLinda(); 
+
+// let mypoints =3 ;
+
+// let add3points = () => {
+//     mypoints += 3 ;
+// };
+// let remmove1Point = () => {
+//     mypoints -= 1 ;
+// };
+// //add3points();
+// add3points();
+// remmove1Point();
+// add3points();
+// add3points();
+// remmove1Point();    
+
+// console.log(mypoints);
+let errorParagraph = document.getElementById("error");
+let purchase = () => {
+    //console.log("button clicked");
+    errorParagraph.textContent= "Something Went Wrong, Please Try again"; 
+
+};
