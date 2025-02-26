3ba407c7f8fde1f4d28e024dfe210a71250b9fcc
mahadi24t <mahadi24t@outlook.com>
2025-02-26 19:05:43 +0600

    calculator app added

--- index.js
+++ index.js
@@ -35,3 +35,29 @@ let purchase = () => {
     errorParagraph.textContent= "Something Went Wrong, Please Try again"; 
 
 };
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el");
+let add = () => {
+    let result = num1 + num2
+    sumEl.textContent = "sum:"+ result
+}
+
+let subtract = () => {
+    let result = num1 - num2
+    sumEl.textContent = "sum:"+ result
+}
+
+let divide = () => {
+    let result = num1 / num2
+    sumEl.textContent = "sum:"+ result
+}
+
+let multiply = () => {
+    let result = num1 * num2
+    sumEl.textContent = "sum:"+ result
+}
