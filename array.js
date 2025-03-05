e9b7c0d47653a6112e4ecb196967e333b8c622ab
mahadi24t <mahadi24t@outlook.com>
2025-03-06 00:08:08 +0600

    learn array

---
+++ array.js
@@ -0,0 +1,22 @@
+// Arrays - ordered lists of items
+
+// 0 indexed
+let featuredPosts = [
+    "Check out my Netflix clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
+
+//featuredPosts
+
+let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+//
+console.log(experience[1])
+console.log(experience[2])
+console.log(experience[0])
+
