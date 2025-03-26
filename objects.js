65fbde8e61914f387d96ad3c2a1c70e55a9dbbc2
mahadi24t <mahadi24t@outlook.com>
2025-03-26 17:47:49 +0600

    object

---
+++ objects.js
@@ -0,0 +1,25 @@
+let course = {
+    title: "Learn CSS Grid for free", 
+    lessons: 16,
+    Creator: "Per Herald Borgen",
+    length: 63,
+    level:2,
+    isFree: true,
+    tags: ["html", "css"]
+
+}
+
+console.log(course.title);
+
+let airbnbCastleList = {
+    title: "Live like a real King in Medival Era",
+    Country: "United States",
+    NumberofGuests: 5,
+    City: "New York",
+    NumberofBedrooms: 3,
+    isAvailable: true,
+    location: ["House no: 12","Road name: Main St" , "Elizabele Castle"]
+
+}
+
+console.log(airbnbCastleList.City, airbnbCastleList.NumberofGuests, airbnbCastleList.location)
