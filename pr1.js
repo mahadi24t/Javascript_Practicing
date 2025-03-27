56520e51e2ddd9a5a6c2bdbd5308d7db0b7b50e5
mahadi24t <mahadi24t@outlook.com>
2025-03-27 07:11:50 +0600

    object

---
+++ pr1.js
@@ -0,0 +1,11 @@
+person = {
+    name: 'Per',
+    age: 35,
+    country: 'Norway'
+}
+
+function logData(){
+    console.log(person.name+ " is "+ person.age+ " years old "+ "and lives in "+ person.country)
+}
+
+logData();
