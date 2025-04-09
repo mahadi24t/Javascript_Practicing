7f57f23462e720091af16169f4da90d27a30d612
mahadi24t <mahadi24t@outlook.com>
2025-04-09 23:57:17 +0600

    lazyday

--- indev.js
+++ indev.js
@@ -3,3 +3,18 @@ let opnBox = document.getElementById('box');
 opnBox.addEventListener('click', function(e) {
     console.log("I want to open the box!");
 });
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// When clicked, render a paragraph under the button (in the container)
+
+// that says "Thank you for buying!"
+function buy() {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Thank you for buying!";
+    container.appendChild(paragraph);
+    
+}
+
+
